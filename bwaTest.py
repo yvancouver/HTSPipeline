4ae324e830caa4be43037dbe0d21e147bface2ed
Yvan Strahm <yvan.strahm@gmail.com>
2011-10-19 16:30:33 +0200

    new version

--- bwaTest.py
+++ bwaTest.py
@@ -2,10 +2,36 @@ import shlex
 import subprocess
 from subprocess import Popen, PIPE, STDOUT
 import os
+import ConfigParser
+import doctest
 
-def bwaVersio():
-    pass
+def Config(file,section):
+    config = ConfigParser.ConfigParser()
+    try:
+        config.readfp(open(file))
+    except:
+        message = "could not find the config file " + file
+        exit(message)
+
+    Version = config.get(section,'Version')
+    Location = config.get(section,'Location')
+    return(Version,Location)
+
+def bwaVersion():
+    r"""Return the samtools version
+    >>> p=Popen(['bwa'], stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+    >>> output = p.communicate()
+    >>> version = output[0].split('\n')[2]
+    >>> print version
+    Version: 0.5.9-r16
+    """
 
 def bwaLocation():
-    pass
+    r"""Test the location of bwa
+    >>> subprocess.check_output(["which", "bwa"]).strip()
+    '/Users/yvans/Home/bin/bwa//bwa'
+    """
+config_samtools=Config('/Users/yvans/Home/workspace/HTSPipeline/config.cfg','Bwa')
 
+#output = doctest.testmod()
+#print "Bwa:\n",output
