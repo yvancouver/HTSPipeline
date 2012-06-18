63c6e68522175646e6339daf22671094d98bac18
yvan <yvan.strahm@gmail.com>
2012-06-18 13:58:57 +0200

    add a test for cuatadapt

---
+++ cutadaptTest.py
@@ -0,0 +1,26 @@
+#import shlex
+import subprocess
+from subprocess import Popen, PIPE, STDOUT
+#import os
+import ConfigParser
+import doctest
+import configReader
+
+
+def cutadaptVersion():
+    r"""Return the cutadapt version Need to comment better NOW
+    >>> configReader.Config(file,section)[0] == subprocess.check_output(["cutadapt", "--version"]).strip()
+    True
+    """
+
+def cutadaptLocation():
+    r"""Test the location of cutadapt
+    >>> cutadaptloc = configReader.Config(file,section)[1]; cutadaptloc == subprocess.check_output(["which", "cutadapt"]).strip()
+    True
+    """
+
+section = 'cutadapt'
+file = '/Users/yvans/Home/workspace/HTSPipeline/config.cfg'
+
+if __name__ == "__main__":
+    doctest.testmod()
